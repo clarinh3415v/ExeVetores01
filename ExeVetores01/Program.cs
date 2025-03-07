91925e009df14f6ca0825b4a43f8041e458e2cec
maria <maria@IDEAPAD_3>
2025-03-06 22:52:51 -0300

    Adicionar arquivos de projeto.

---
+++ ExeVetores01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+int N, P = 0;
+double M = 0;
+double[] vet;
+
+N = int.Parse(Console.ReadLine());
+vet = new double[N];
+
+string[] s = Console.ReadLine().Split(' ');
+
+for (int i = 0; i < N; i++) { 
+    vet[i] = double.Parse(s[i], System.Globalization.CultureInfo.InvariantCulture);
+}
+
+for
+    (int i = 0; i <N; i++)
+{
+    if (vet[i] > M)
+    {
+        M = vet[i];
+        P = i;
+    }
+}
+ Console.WriteLine(M.ToString("F1",CultureInfo.InvariantCulture ));
+Console.WriteLine(value: P);
+
+Console.ReadLine();
